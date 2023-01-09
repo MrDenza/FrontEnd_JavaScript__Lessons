6bbe69bd44eb79cbdbe23d9406dc5f1aea7084d7
MrDenza <nikolai_dudevich@mail.ru>
2023-01-09 20:47:43 +0300

    N23.v3

--- JS_1.js
+++ JS_1.js
@@ -12,7 +12,6 @@ let formDef1=
 		{label:'Описание сайта:',kind:'memo',name:'description'},
 		{caption:'Опубликовать',kind:'submit'},
 	];
-
 let formDef2=
 	[
 		{label:'Фамилия:',kind:'longtext',name:'lastname'},
@@ -21,26 +20,16 @@ let formDef2=
 		{label:'Возраст:',kind:'number',name:'age'},
 		{caption:'Зарегистрироваться',kind:'submit'},
 	];
+
 let formInSite1 = document.getElementById("test1");
-let formInSite2 = document.getElementsByClassName("test2");
+let formInSite2 = document.getElementById("test2");
 generateForm(formInSite1, formDef1);
 generateForm(formInSite2, formDef2);
 
-/*function generateForm(formIn, formInfo) {
-	for (let formInfoEl of formInfo) {
-		for (let formInfoElKey in formInfoEl) {
-			console.log(formInfoElKey);
-			let el = formElement(formInfoElKey, formInfoEl[formInfoElKey]);
-			console.log(el);
-			formIn.appendChild(el);
-		}
-	}
-}*/
 function generateForm(formIn, formInfo) {
 	for (let formInfoEl of formInfo) {
 		let addEl = '';
 		let newString = document.createElement('br');
-		formIn.appendChild(newString);
 		if('label' in formInfoEl){
 			addEl = document.createElement('span');
 			addEl.textContent = formInfoEl['label'];
@@ -51,12 +40,10 @@ function generateForm(formIn, formInfo) {
 			if (i === 'longtext' || i === 'number' || i === 'shorttext'){
 				addEl = document.createElement('input');
 				addEl.setAttribute('type', 'text');
-				addEl.setAttribute('name', formInfoEl['name']);
-				formIn.appendChild(addEl);
-				continue;
 			}
 			if (i === 'combo'){
 				addEl = document.createElement('select');
+				addEl.setAttribute('title','list');
 				if('variants' in formInfoEl){
 					let variantInfo = formInfoEl['variants'];
 					for (let varPos of variantInfo) {
@@ -66,79 +53,40 @@ function generateForm(formIn, formInfo) {
 						addEl.appendChild(pos);
 					}
 				}
-				addEl.setAttribute('name', formInfoEl['name']);
-				formIn.appendChild(addEl);
-				continue;
 			}
-			if (i === 'radio' && 'variants' in formInfoEl){ //variants:[{text:'бесплатное',value:1}
+			if (i === 'radio' && 'variants' in formInfoEl){
 				let radioInfo = formInfoEl['variants'];
 				for (let radioPos of radioInfo) {
 					addEl = document.createElement('input');
 					addEl.setAttribute('type', 'radio');
 					addEl.setAttribute('value', radioPos['value']);
-					addEl.setAttribute('name', 'radio');
-					let addElText = document.createElement('span');
+					addEl.setAttribute('name', formInfoEl['name']);
+					let addElText = document.createElement('i');
 					addElText.textContent = radioPos['text'];
 					formIn.appendChild(addEl);
 					formIn.appendChild(addElText);
 				}
+				formIn.appendChild(newString);
 				continue;
 			}
 			if (i === 'check'){
 				addEl = document.createElement('input');
 				addEl.setAttribute('type', 'checkbox');
-				addEl.setAttribute('name', formInfoEl['name']);
-				formIn.appendChild(addEl);
-				continue;
 			}
 			if (i === 'memo'){
-				//formIn.appendChild(newString);
 				addEl = document.createElement('textarea');
-				addEl.setAttribute('name', formInfoEl['name']);
-				//formIn.appendChild(newString);
-				formIn.appendChild(addEl);
-				//continue;
+				
 			}
-			//addEl.setAttribute('name', formInfoEl['name']);
-			//formIn.appendChild(addEl);
-			//formIn.appendChild(document.createElement('br'));
+			if (i === 'submit'){
+				addEl = document.createElement('input');
+				addEl.setAttribute('type', 'submit');
+				addEl.setAttribute('value', formInfoEl['caption']);
+			}
+			if ('name' in formInfoEl){
+				addEl.setAttribute('name', formInfoEl['name']);
+			}
+			formIn.appendChild(addEl);
 		}
+		formIn.appendChild(newString);
 	}
 }
-
-/*function formElement(formTag, formVal) {
-	let addElement = '';
-	if(formTag === 'label'){
-		addElement = document.createElement('span');
-		addElement.textContent = formVal;
-	}
-	if(formTag === 'kind'){
-		switch (formVal) {
-			case 'longtext':
-				addElement = document.createElement('span');
-				break;
-			case 'number':
-
-				break;
-			case 'shorttext':
-
-				break;
-			case 'combo':
-
-				break;
-			case 'radio':
-
-				break;
-			case 'check':
-
-				break;
-			case 'memo':
-
-				break;
-			case 'submit':
-
-				break;
-		}
-	}
-	return addElement;
-}*/
