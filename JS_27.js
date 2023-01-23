0a1f53d39e8c68b13c8da4e7a5cfa120da269cb4
MrDenza <nikolai_dudevich@mail.ru>
2023-01-23 20:30:53 +0300

    N27.v1

---
+++ JS_27.js
@@ -0,0 +1,16 @@
+"use strict"; // Строгий режим
+// ----------------------- JavaScript -----------------------
+let formInSite =  document.forms['form'];
+
+let elDeveloper = formInSite.elements['developer'];
+let elName = formInSite.elements['name'];
+let elSite = formInSite.elements['site'];
+let elDate = formInSite.elements['date'];
+let elUsers = formInSite.elements['users'];
+let elEmail = formInSite.elements['email'];
+let elList = formInSite.elements['list'];
+let elType = formInSite.elements['type'];
+let elCheck = formInSite.elements['check'];
+let elText = formInSite.elements['text'];
+let elRun = formInSite.elements['run'];
+
