92539973a5b668ad7a8e04676ab6b585b86dc078
MrDenza <nikolai_dudevich@mail.ru>
2023-01-23 20:31:21 +0300

    N27.v2

--- JS_27.js
+++ JS_27.js
@@ -14,3 +14,39 @@ let elCheck = formInSite.elements['check'];
 let elText = formInSite.elements['text'];
 let elRun = formInSite.elements['run'];
 
+elDeveloper.addEventListener('blur',validateDev('developer',elDeveloper),false);
+function validateDev(valType, valEl) {
+	return function(EO) {
+		EO = EO || window.event;
+		console.log('Событие: '+ EO);
+		console.log('Тип валидации: '+ valType);
+		console.log('Элемент: '+ valEl);
+		
+		if (valType === 'developer') {
+			if (valEl.value.length === 0){
+				console.log('err');
+				statusPrint(valEl, true);
+			}
+			else{
+				statusPrint(valEl, false);
+				console.log('good');
+			}
+		}
+	}
+}
+
+function statusPrint(el, err){
+	let editEL = el.parentNode.getElementsByTagName('i');
+	if (err === true) {
+		editEL[0].style.color = 'red';
+		editEL[0].textContent = 'Error!';
+	}
+	else if (err === false) {
+		editEL[0].style.color = 'green';
+		editEL[0].textContent = 'Okay!';
+	}
+	//ПОЛЕЗНЫЙ КОД!
+	/*let newEl = document.createElement("i"); 
+	(err === true) ? newEl.textContent = "Error!" : newEl.textContent = "Ok!";
+	el.parentNode.insertBefore(newEl, el.nextSibling);*/
+}
