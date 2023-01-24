202ac405b6b9bed06c350d2f6b6fc5c3f24dec98
MrDenza <nikolai_dudevich@mail.ru>
2023-01-24 20:30:24 +0300

    N27.v5

--- JS_27.js
+++ JS_27.js
@@ -14,7 +14,6 @@ let elCheck = formInSite.elements['check'];
 let elText = formInSite.elements['text'];
 
 let elDeveloperErr, elNameErr, elSiteErr, elDateErr, elUsersErr, elEmailErr, elListErr, elTypeErr, elCheckErr, elTextErr;
-elDeveloperErr = elNameErr = elSiteErr = elDateErr = elUsersErr = elEmailErr = elListErr = elTypeErr = elCheckErr = elTextErr = true;
 let checkErr = {'developer':elDeveloper,'name':elName,'site':elSite,'date':elDate,'users':elUsers,'email':elEmail,'list':elList,'type':elType,'check':elCheck,'text':elText};
 
 elDeveloper.addEventListener('blur',validateForm('developer',elDeveloper),false);
@@ -24,9 +23,9 @@ elDate.addEventListener('blur',validateForm('date',elDate),false);
 elUsers.addEventListener('blur',validateForm('users',elUsers),false);
 elEmail.addEventListener('blur',validateForm('email',elEmail),false);
 elList.addEventListener('change',validateForm('list',elList),false);
-elType[0].addEventListener('click',validateForm('type',elType[0]),false);
-elType[1].addEventListener('click',validateForm('type',elType[1]),false);
-elType[2].addEventListener('click',validateForm('type',elType[2]),false);
+elType[0].addEventListener('mousedown',validateForm('type',elType[0]),false);
+elType[1].addEventListener('mousedown',validateForm('type',elType[1]),false);
+elType[2].addEventListener('mousedown',validateForm('type',elType[2]),false);
 elCheck.addEventListener('change',validateForm('check',elCheck),false);
 elText.addEventListener('blur',validateForm('text',elText),false);
 formInSite.addEventListener('submit',sendForm,false);
@@ -34,16 +33,25 @@ formInSite.addEventListener('submit',sendForm,false);
 function validateForm(valType, valEl) {
 	return function(EO) {
 		EO = EO || window.event;
-		console.log('Валидация поля: '+ valType);
+		//console.log('Валидация поля: '+ valType);
 		valEl.value = valEl.value.trim(); // чистим проблемы в начале и конце
 		//------------------------------------------------------------------
 		if (valType === 'developer' || valType === 'name' || valType === 'site') {
+			let err = false;
 			if (valEl.value.length < 1) {
 				statusPrint(valEl, true, 'Заполните поле корректно! Минимальная длина: 1 символ.');
-				return;
+				err = true;
 			}
 			if (valEl.value.length > 50) {
 				statusPrint(valEl, true, 'Превышена максимальная длина! Максимальная длина: 50 символов.');
+				err = true;
+			}
+			if (err === true){
+				switch (valType){
+					case 'developer': elDeveloperErr = true; break;
+					case 'name': elNameErr = true; break;
+					case 'site': elSiteErr = true; break;
+				}
 				return;
 			}
 			statusPrint(valEl, false);
@@ -57,15 +65,17 @@ function validateForm(valType, valEl) {
 		if (valType === 'date') {
 			if (valEl.value.length === 0) {
 				statusPrint(valEl, true, 'Заполните дату!');
+				elDateErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
-			elDeveloperErr = false;
+			elDateErr = false;
 		}
 		//------------------------------------------------------------------
 		if (valType === 'users') {
 			if (Number(valEl.value) === 0) {
 				statusPrint(valEl, true, 'Введите корректное значение! Минимум: 1.');
+				elUsersErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
@@ -75,10 +85,12 @@ function validateForm(valType, valEl) {
 		if (valType === 'email') {
 			if (valEl.value.length === 0) {
 				statusPrint(valEl, true, 'Введите Ваш Email!');
+				elEmailErr = true;
 				return;
 			}
 			if (valEl.value.length < 5 || valEl.value.indexOf('@') === -1 || valEl.value.indexOf('.') === -1) {
 				statusPrint(valEl, true, 'Ваш Email не корректный! Пример: w@ro.ru');
+				elEmailErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
@@ -86,9 +98,9 @@ function validateForm(valType, valEl) {
 		}
 		//------------------------------------------------------------------
 		if (valType === 'list') {
-			console.log(valEl.value);
 			if (Number(valEl.value) === 0) {
 				statusPrint(valEl, true, 'Выберите рубрику каталога!');
+				elListErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
@@ -96,13 +108,18 @@ function validateForm(valType, valEl) {
 		}
 		//------------------------------------------------------------------
 		if (valType === 'type') {
-			if (valEl.value === '') {
-				statusPrint(valEl, true, 'Выберите способ размещения!');
-				return;
+			if (valEl.length > 1){
+				if (valEl.value === '') {
+					statusPrint(valEl[0], true, 'Выберите способ размещения!');
+					elTypeErr = true;
+					return;
+				}
+				let z = Number(valEl.value);
+				valEl = valEl[z];
 			}
-			alert('a');
-			if (Number(valEl.value) === 2 || Number(valEl.value) === 3) {
+			if (Number(valEl.value) === 1 || Number(valEl.value) === 2) {
 				statusPrint(valEl, true, 'Платное или VIP размещение доступно после покупки подписки!');
+				elTypeErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
@@ -112,6 +129,7 @@ function validateForm(valType, valEl) {
 		if (valType === 'check') {
 			if (valEl.checked === false ) {
 				statusPrint(valEl, true, 'Отключить отзывы можно только платно либо по подписке \"VIP\"!');
+				elCheckErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
@@ -121,12 +139,12 @@ function validateForm(valType, valEl) {
 		if (valType === 'text') {
 			if (valEl.value.length < 10 || valEl.value.length > 150) {
 				statusPrint(valEl, true, 'Заполните поле корректно! Длина описания от 10 до 150 символов.');
+				elTextErr = true;
 				return;
 			}
 			statusPrint(valEl, false);
-			elTypeErr = false;
+			elTextErr = false;
 		}
-		EO.preventDefault();
 	}
 }
 
@@ -136,12 +154,12 @@ function statusPrint(el, err, infoText){
 	if (err === true) {
 		editEL[0].style.color = 'red';
 		editEL[0].textContent = 'Error! '+text;
-		console.log('Ошибка!');
+		//console.log('Ошибка!');
 	}
 	else if (err === false) {
 		editEL[0].style.color = 'green';
 		editEL[0].textContent = 'Okay! '+text;
-		console.log('Пройдена!');
+		//console.log('Пройдена!');
 	}
 	//ПОЛЕЗНЫЙ КОД!
 	/*let newEl = document.createElement("i"); 
@@ -154,10 +172,11 @@ function sendForm(EO) {
 	for (let checkErrKey in checkErr) {
 		validateForm(checkErrKey, checkErr[checkErrKey])(EO);
 	}
-	if (elTextErr === false){
-		console.log('lol');
+	//console.table(elDeveloperErr, elNameErr, elSiteErr, elDateErr, elUsersErr, elEmailErr, elListErr, elTypeErr, elCheckErr, elTextErr);
+	if ((elDeveloperErr || elNameErr || elSiteErr || elDateErr || elUsersErr || elEmailErr || elListErr || elTypeErr || elCheckErr || elTextErr) === false){
+		alert('Отправить форму?');
+		return;
 	}
-	
 	EO.preventDefault();
 	return false;
 }
