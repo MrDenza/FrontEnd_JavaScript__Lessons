d362c6af77f73d01d726521a464c8162b8e663b5
MrDenza <nikolai_dudevich@mail.ru>
2023-01-30 22:54:30 +0300

    N30.v1

---
+++ JS_30.js
@@ -0,0 +1,38 @@
+"use strict"; // Строгий режим
+// ----------------------- JavaScript -----------------------
+window.addEventListener('load',startPosElem,false);
+let zInd = 0;
+function startPosElem() {
+	let imgAll =  document.getElementsByTagName('img');
+	for (let i = imgAll.length - 1; i > -1; i--) {
+		imgAll[i].style.top = imgAll[i].offsetTop + 'px';
+		imgAll[i].style.left = imgAll[i].offsetLeft + 'px';
+		//imgAll[i].style.position = 'absolute'; 
+		// при указании в этом месте position, значения top изменяются почему-то
+	}
+	for (let i = imgAll.length - 1; i > -1; i--) {
+		imgAll[i].style.position = 'absolute';
+	}
+}
+
+document.onmousedown = function(eo) {
+	eo = eo || window.event;
+	let selectElem = eo.target;
+	if (selectElem.tagName === 'IMG') {
+		let clickMouseX = eo.pageX - selectElem.offsetLeft;
+		let clickMouseY = eo.pageY - selectElem.offsetTop;
+		selectElem.style.zIndex = zInd;
+		window.onmousemove = function(eo){
+			eo.preventDefault();
+			selectElem.style.top = (eo.pageY - clickMouseY) + 'px';
+			selectElem.style.left = (eo.pageX - clickMouseX) + 'px';
+		}
+		selectElem.onmouseup = function(eo) { 
+			window.onmousemove = null;
+			zInd++;
+		}
+	}
+}
+
+
+
